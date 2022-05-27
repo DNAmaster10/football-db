9dff56153210f0f3457d34c065c768c4db892879
DNAmaster10 <44494235+DNAmaster10@users.noreply.github.com>
2022-05-27 21:10:35 +0100

    Update mariadb_test.py

--- mariadb_test.py
+++ mariadb_test.py
@@ -1,17 +1,6 @@
 import mariadb
 import sys
-
-try:
-    conn = mariadb.connect(
-        user = "kaloro-db",
-        password = "password",
-        host = "localhost",
-        database = "football-stats"
-    )
-    print ("Connected")
-except mariadb.Error as e:
-    print ("Could not connect to the database: " + e)
-    sys.exit(1)
+import "./web-scrape/conn.py"
 
 print ("got to 1")
 cur = conn.cursor()
