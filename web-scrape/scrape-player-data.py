569816ec9e24e2133cefdf2967ca9e32bdd54283
DNAmaster10 <louishollobon@outlook.com>
2022-06-25 23:01:40 +0100

    update

--- web-scrape/scrape-player-data.py
+++ web-scrape/scrape-player-data.py
@@ -14,3 +14,13 @@ try:
 except mariadb.Error as e:
     print ("Could not connect to the database")
     sys.exit(1)
+
+statement = "SELECT firstname,lastname,link FROM player_links"
+conn.commit()
+cur.execute(statement)
+row = cur.fetchone()
+if (row == None):
+    print ("No players found in db")
+else:
+    for (firstName, lastName, link) in cur:
+        print (f"First Name: {firstName}, Last Name: {lastName}")
