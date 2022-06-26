634a5054e4d6670b2e6e83549311ad6c69b59974
DNAmaster10 <louishollobon@outlook.com>
2022-06-26 01:11:49 +0100

    update

--- web-scrape/scrape-player-data.py
+++ web-scrape/scrape-player-data.py
@@ -2,6 +2,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 import mariadb
 import sys
+import time
 
 try:
     conn = mariadb.connect(
@@ -23,5 +24,27 @@ row = cur.fetchone()
 if (row == None):
     print ("No players found in db")
 else:
-    for (firstName, lastName, link) in cur:
+    for (firstName, lastName, link) in cur.fetchall():
+        time.sleep(3)
         print (f"First Name: {firstName}, Last Name: {lastName}")
+        theurl = link
+        thepage = urllib.request.urlopen(theurl)
+        soup = BeautifulSoup(thepage)
+        for strong in soup.find_all("strong", string="Club:"):
+            parent_tag = strong.parent
+            for a in parent_tag.find_all("a"):
+                club = a.encode_contents()
+                club = str(club)
+                club = club.replace("b", "", 1)
+                club = club.replace("'", "")
+                statement = "SELECT firstname,lastname FROM players WHERE firstname=%s AND lastname=%s"
+                data = (firstName, lastName)
+                conn.commit()
+                cur.execute(statement, data)
+                rowt = cur.fetchone()
+                if (rowt == None):
+                    statement = "INSERT INTO players (club,firstname,lastname) VALUES (%s,%s,%s)"
+                    data = (club,firstName,lastName)
+                    conn.commit()
+                    cur.execute(statement, data)
+                print (club)
